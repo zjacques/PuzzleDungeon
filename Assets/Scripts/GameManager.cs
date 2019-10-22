83d10f53bc7b16c3beab713bcfe1546f28514e9e
Zachary <zachary.jacques160@gmail.com>
2019-10-22 14:52:40 -0400

    added gp and counting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    GameObject currentParty;
+    GameObject currentGrid;
+
+    public Text GPdisplay;
+
+    int GP = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GPdisplay.text = GP.ToString();
+        currentParty = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int mapLayerMask = 1 << 8;
+        Collider2D thisRoom = Physics2D.OverlapPoint(currentParty.transform.position, mapLayerMask);
+
+        if(thisRoom.gameObject!=null)
+        {
+            foreach(Transform child in thisRoom.transform)
+            {
+                if(child.CompareTag("Gold"))
+                {
+                    Destroy(child.gameObject);
+                    GP++;
+                }
+            }
+        }
+
+        GPdisplay.text = GP.ToString();
     }
 }
